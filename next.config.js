957c93a65a3550e5edc869590199dad5bf8bc784
Remy004 <remyazeh@gmail.com>
2026-07-22 16:41:13 -0500

    Upgrade Next.js and secure dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { serverComponentsExternalPackages: ['bullmq', 'ioredis'] }
+  serverExternalPackages: ['bullmq', 'ioredis']
 };
+
 module.exports = nextConfig;
